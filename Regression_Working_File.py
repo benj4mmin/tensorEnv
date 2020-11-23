ad7f4daf134d49447c6f3af5cdd0a7d5a046afdd
benjamin yurchison <yurchisonben@gmail.com>
2020-11-22 19:05:22 -0500

    tensorflow with lin reg algo
    tensorflow with linear regression

---
+++ Regression_Working_File.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+data = pd.read_csv("student-mat.csv", sep=";")
+print(data.head())
+
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+print(data.head())
+
+#predict = "G3"
+
+# X = np.array(data.drop([predict], 1)) #features
+# y = np.array(data[predict]) #labels
+
+# x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
+
+# linear = linear_model.LinearRegression()
+
+# limear.fit(x_train, y_train
+
+# acc = linear.score(x_test, y_test) # accuracy
+
+# print(acc)
+
+# print('Coefficient: \n', linear.coef_) # each slope val
+# print('Intercept: \n', linear.intercept_) #intercept
+
+# predictions = linear.predict(x_test)
+
+# for x in range(len(predictions));
+#   print(predictions[x], x_test[x], y_test[x])
